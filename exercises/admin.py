3de5abe80a7db09fc8331a602ae93c0cb4832093
louispawaon <lpawaon@gmail.com>
2023-05-02 10:42:19 +0800

    Exercise 5: New

--- exercises/admin.py
+++ exercises/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
+from exercises.models import (Book, Author, Classification, Publisher)
+
+class BookInLine(admin.TabularInline):
+    model = Book
+    extra = 3
+
+class BookAdmin(admin.ModelAdmin):
+    fieldsets=[
+        (None, {'fields': ['title', 'authors','publisher', 'publication_date']}),
+    ]
+
+class AuthorAdmin(admin.ModelAdmin):
+    fieldsets=[
+        (None, {'fields':['first_name', 'last_name']}),
+        ('e-mail', {'fields':['email']})
+    ]
+    search_fields = ["first_name", "last_name"]
+    list_display = ['email']
+
+class PublisherAdmin(admin.ModelAdmin):
+    fieldsets=[
+        (None,{'fields':['name', 'city', 'country', 'website']})
+    ]
+    search_fields = ['name', 'city', 'country', 'website']
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Publisher, PublisherAdmin)
+admin.site.register(Classification)
+
 # Register your models here.
--- exercises/models.py
+++ exercises/models.py
@@ -1,25 +1,69 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from datetime import timedelta
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=30)
+    city = models.CharField(max_length=60)
+    state_province = models.CharField(max_length=30)
+    country = models.CharField(max_length=50)
+    website = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Publisher")
+        verbose_name_plural = _("Publishers")
 
 class Classification (models.Model):
     code = models.CharField(max_length=3)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=50)
     description = models.TextField()
+    #book = models.OneToOneField('exercises.Book', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
     
 class Book (models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey('Author', on_delete=models.CASCADE)
-    classification = models.ForeignKey('Classification', on_delete=models.CASCADE)
+    authors = models.ManyToManyField('exercises.Author')
+    publisher = models.ForeignKey(
+        "exercises.Publisher", 
+        on_delete=models.CASCADE
+    )
+
+    
+    publication_date = models.DateField()
+        
+    def was_published_recently(self):
+        date_today = timezone.now().date()
+        return self.publication_date >= date_today - timedelta(day=1)
+
+    was_published_recently.admin_order_field = "publication_date"
+    was_published_recently.boolean=True
+    was_published_recently.short_description = "Published recently?"
+    
 
     def __str__(self):
         return self.title
     
+    class Meta:
+        verbose_name = _("Book")
+        verbose_name_plural = _("Books")
+
 class Author (models.Model):
-    name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(verbose_name="e-mail")
+
+    class Meta:
+        verbose_name = _("Author")
+        verbose_name_plural = _("Authors")
 
     def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.last_name}"
 
 # Create your models here.
